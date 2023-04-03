cfae579b477cf5a66b297d56128546ac5fe3360a
Cris <cris@crisalb.com>
2023-04-03 10:47:41 -0700

    test

--- infra/modules/ec2/ec2.tf
+++ infra/modules/ec2/ec2.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22    
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["76.22.12.11/32"] 
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
